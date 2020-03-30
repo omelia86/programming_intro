ec93fb2e7b5687e7ce5e59158fbd4ecc417a449a
altair961 <altair961@users.noreply.github.com>
2020-03-30 20:47:15 +0400

    Added Animals project

---
+++ Animals/Animals/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog sharik = new Dog();
+            sharik.LegsAmount = 4;
+            sharik.Bark();
+        }
+    }
+}
