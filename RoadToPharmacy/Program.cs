8b69ef4aed1af5d09524503f6edfd4c63ec15d3e
omelia86 <gulnaz.umitbaeva@gmail.com>
2020-03-31 19:47:56 +0300

    Added RoadToPharmacy

---
+++ RoadToPharmacy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoadToPharmacy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
