7bdf5ba742b89e1deea9d787776eeab369200fb2
omelia86 <gulnaz.umitbaeva@gmail.com>
2020-04-01 23:20:11 +0300

    Created Greetings project

---
+++ Greetings/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+
+    int age = 42;
+    cout << "your age is " << age << "\n";
+    string name = "Niki";
+    cout <<" Hello " << name<< "\n";
+
+    return 0;
+}
