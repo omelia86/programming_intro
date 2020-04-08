f33fcddf1bdc6285d0600c7664b842e41d2ad997
omelia86 <gulnaz.umitbaeva@gmail.com>
2020-04-08 16:58:55 +0300

    Modified Hello Ex

--- ch2_dr1_hello_world/main.cpp
+++ ch2_dr1_hello_world/main.cpp
@@ -3,12 +3,16 @@
 #include<vector>
 #include<algorithm>
 #include<cmath>
+
 using namespace std;
 
 int main()//
 
 {
-    cout <<"Hello_World!\n";
+    cout <<"Hello,programming!\n";
+    cout <<"Here we go!\n\n";
+
+
 
     return 0;
 
