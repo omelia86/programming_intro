ddcf4d0fb0fec38a7f0923361acfe5c2b6f0b187
omelia86 <gulnaz.umitbaeva@gmail.com>
2020-04-13 22:34:02 +0300

    Created ch2_variable_types_example project

---
+++ ch2_variable_types_example/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace  std;
+
+int main()
+{
+    string first_name;
+    first_name = "Gulnaz";
+    cout <<first_name;
+    cout<<"\n";
+    first_name = "Nici";
+    cout <<first_name;
+    cout<<"\n";
+
+    int age;
+    age = 45;
+    cout <<age;
+    cout<<"\n";
+    age = 15 + 20;
+    cout<<age;
+    cout<<"\n";
+
+    int books_amount;
+
+    cout <<books_amount;
+    cout <<"\n";
+
+    char seat_sector;
+    seat_sector = 'A' + 7;
+    cout << seat_sector;
+    cout << "\n";
+
+
+
+
+}
